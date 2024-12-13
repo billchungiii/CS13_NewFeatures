622b436080705f33c9b88a843d17ac7c34a8c426
Bill Chung <bill_chungiii@hotmail.com>
2024-12-14 04:55:47 +0800

    add samples

---
+++ Async_Ref_Sample001/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Async_Ref_Sample001
+{
+    internal class Program
+    {
+        static int[] storage = new int[10] {21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+        async static Task Main(string[] args)
+        {
+            await Task.Delay(100);
+            ref int source = ref GetValue();
+            source *= 2;
+            Console.WriteLine(storage[0]);
+        }
+        static ref int GetValue()
+        {            
+            return ref storage[0];
+        }
+    }
+}
---
+++ Enumerable_Ref_Sample001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Enumerable_Ref_Sample001
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+
+          
+        }
+      
+    }
+
+
+}
