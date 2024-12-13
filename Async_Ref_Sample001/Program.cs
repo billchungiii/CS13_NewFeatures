0dfb98e115bf3fadfe516e74dbbb7333e2dbc63a
Bill Chung <bill_chungiii@hotmail.com>
2024-12-14 07:05:04 +0800

    modify sample

--- Async_Ref_Sample001/Program.cs
+++ Async_Ref_Sample001/Program.cs
@@ -2,21 +2,29 @@
 {
     /// <summary>
     /// C# 13.0, .NET 9.0
-    /// ref in async method
+    /// ref in async / iterator method
     /// </summary>
     internal class Program
     {
-        static int[] storage = new int[10] {21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+        static int[] numbers = new int[10] {21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
         async static Task Main(string[] args)
         {
             await Task.Delay(100);
             ref int source = ref GetValue();
             source *= 2;
-            Console.WriteLine(storage[0]);
+            Console.WriteLine(numbers[0]);
         }
         static ref int GetValue()
         {            
-            return ref storage[0];
+            return ref numbers[0];
+        }
+
+        static IEnumerable<int> GetNumbers()
+        {
+            ref int x = ref numbers[0];
+            yield return 1;
+            yield return 2;
+            yield return 3;
         }
     }
 }
